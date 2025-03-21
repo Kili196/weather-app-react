9636d9e53bd9b6105ce5639a6860c196fb555eb0
kili196 <kilian.baier16@gmail.com>
2025-03-21 13:04:50 +0100

    add grid to main container
    add styling to card component
    
    add drop shadow to card component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,20 @@
 import "../style/main.css";
 
+import ClockCard from "../components/cards/ClockCard";
+
 const Main = ({ data }) => {
   console.log(data);
 
-  return <main>{data.location.country}</main>;
+  return (
+    <main>
+      <ClockCard />
+      <ClockCard />
+      <ClockCard />
+      <ClockCard />
+      <ClockCard />
+      <ClockCard />
+    </main>
+  );
 };
 
 export default Main;
--- src/components/cards/ClockCard.jsx
+++ src/components/cards/ClockCard.jsx
@@ -1,3 +1,7 @@
-const ClockCard = () => {
-  return <> </>;
+import "../../style/card.css";
+
+const ClockCard = ({ data }) => {
+  return <div className="card card-padding clock-card"> 13:04</div>;
 };
+
+export default ClockCard;
