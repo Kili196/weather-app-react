32b73d8f8b9b460ffb8a354898a1fb8b505b1616
kili196 <kilian.baier16@gmail.com>
2025-03-29 17:30:07 +0100

    add container to main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,41 +3,44 @@ import "../style/main.css";
 import ClockCard from "../components/cards/ClockCard";
 import LocationInformationCard from "./cards/LocationInformationCard";
 import SingleInformationCard from "./cards/SingleInformationCard";
+import Container from "./Container";
 
 const Main = ({ data }) => {
   console.log(data);
   return (
     <main>
-      <header>
-        <ClockCard data={data} />
-      </header>
-      <section>
-        <div className="left-side">
-          <div className="general-information-section">
-            <LocationInformationCard
-              mainTextLeft={data.location.name}
-              secondaryTextLeft={data.location.country}
-              mainTextRight={`${data.current.temp_c}°`}
-              secondaryTextRight={`${data.current.temp_f}F`}
-            />
+      <Container>
+        <header>
+          <ClockCard data={data} />
+        </header>
+        <section>
+          <div className="left-side">
+            <div className="general-information-section">
+              <LocationInformationCard
+                mainTextLeft={data.location.name}
+                secondaryTextLeft={data.location.country}
+                mainTextRight={`${data.current.temp_c}°`}
+                secondaryTextRight={`${data.current.temp_f}F`}
+              />
 
-            <div className="detailed-information-section">
-              <SingleInformationCard
-                headline={"Huminidty"}
-                information={data.current.humidity}
-                unit={"%"}
-              />
-              <SingleInformationCard
-                headline={"Wind-speed"}
-                information={data.current.wind_kph}
-                unit={"km/h"}
-              />
+              <div className="detailed-information-section">
+                <SingleInformationCard
+                  headline={"Huminidty"}
+                  information={data.current.humidity}
+                  unit={"%"}
+                />
+                <SingleInformationCard
+                  headline={"Wind-speed"}
+                  information={data.current.wind_kph}
+                  unit={"km/h"}
+                />
+              </div>
             </div>
+            <div> dsads</div>
           </div>
-          <div> dsads</div>
-        </div>
-        <div className="right-side"></div>
-      </section>
+          <div className="right-side"></div>
+        </section>
+      </Container>
     </main>
   );
 };
