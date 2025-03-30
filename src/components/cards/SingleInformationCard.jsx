edff764ab344e7d67e28cec184cc9c450191acec
kili196 <kilian.baier16@gmail.com>
2025-03-30 17:06:13 +0200

    add card styling

--- src/components/cards/SingleInformationCard.jsx
+++ src/components/cards/SingleInformationCard.jsx
@@ -1,10 +1,10 @@
 const SingleInformationCard = ({ headline, information, unit }) => {
   return (
     <>
-      <div className="card flex-grow">
+      <div className="card flex-grow single-information-card">
         <h1 className="single-information-headline "> {headline}</h1>
-        <div className="">
-          <label className="">
+        <div className="flex-grow center">
+          <label className="single-information-detail">
             {information} <label className=""> {unit}</label>
           </label>
         </div>
