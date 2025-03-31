f65bcfcd5560f748eac532148d56d5fabdb47b26
kili196 <kilian.baier16@gmail.com>
2025-03-31 22:01:17 +0200

    format time correct

--- src/components/cards/ForecastCard.jsx
+++ src/components/cards/ForecastCard.jsx
@@ -1,15 +1,20 @@
 const ForecastcastCard = ({ heading, forcastData }) => {
   console.log(forcastData);
+
+  const regex = /([\d]+:[\d]{2})(:[\d]{2})(.*)/;
+  const replacement = "$1$3";
+
   return (
     <>
       <div className="card forcast-card">
         <h1 className="card-sub-heading"> {heading}</h1>
         <section className="forcast-section flex-grow">
           {forcastData.hour.map((element) => {
+            const date = new Date(element.time);
             return (
               <li>
-                {" "}
-                {element.time} {element.condition.text}{" "}
+                {date.toLocaleTimeString().replace(regex, replacement)}{" "}
+                {element.condition.text}{" "}
                 {<img src={element.condition.icon} width={50} />}
               </li>
             );
